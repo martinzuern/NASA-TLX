ea87b4b7dfa1ea7535c72eae00c52578dbdd0f89
Martin Zürn <martin@weltundall.de>
2017-01-27 00:17:57 +0100

    Made that participants are ignored

--- config.php
+++ config.php
@@ -1,7 +1,6 @@
 <?php
 // MODIFY THIS:
 $results_folder_path = 'results'; // best kept outside www root
-$dynamic_participants = true;
-$participants = array(
-  'laurens'
-);
+$dynamic_participants = false;
+$check_ids = false;
+$participants = array();
