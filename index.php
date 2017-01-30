ccd0cd2c72673ee6e4990e0d41b1e33db7af6784
Martin Zuern <martin@weltundall.de>
2017-01-30 15:16:24 +0100

    Merged languages into locales

--- index.php
+++ index.php
@@ -4,7 +4,20 @@ $token = md5(uniqid(rand(), TRUE));
 $_SESSION['token'] = $token;
 $_SESSION['token_time'] = time();
 
-require_once('config.php');?>
+// Specify location of translation tables
+// Set language to German
+putenv('LC_ALL=de_DE');
+setlocale(LC_ALL, 'de_DE');
+bindtextdomain("Nasa-TLX", "./locale");
+textdomain("Nasa-TLX");
+
+require_once('config.php');
+
+if(!$dynamic_participants AND (!isset($_GET['id']) OR strlen($_GET['id']) < 1)) {
+  die(gettext("You have to submit an ID."));
+}
+
+?>
 <html>
   <head>
     <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
@@ -24,7 +37,6 @@ require_once('config.php');?>
     -->
 
     <script language="JavaScript" type="text/javascript">
-    <!--
       // Participant vars
       var participant_id = "<?php echo $_GET['id']; ?>";
       var token = "<?php echo $token; ?>";
@@ -36,38 +48,37 @@ require_once('config.php');?>
       var def = new Array();
       var NUM_SCALES = 6;
 
-      scale[0] = "Mental Demand";
-      left[0] = "Low";
-      right[0] = "High";
-      def[0] = "How much mental and perceptual activity was required (e.g. thinking, deciding, calculating, remembering, looking, searching, etc)? Was the task easy or demanding, simple or complex, exacting or forgiving?";
+      scale[0] = '<?= gettext("Mental Demand") ?>';
+      left[0] = '<?= gettext("Low") ?>';
+      right[0] = '<?= gettext("High") ?>';
+      def[0] = '<?= gettext("How much mental and perceptual activity was required (e.g. thinking, deciding, calculating, remembering, looking, searching, etc)? Was the task easy or demanding, simple or complex, exacting or forgiving?") ?>';
 
-      scale[1] = "Physical Demand";
-      left[1] = "Low";
-      right[1] = "High";
-      def[1] = "How much physical activity was required (e.g. pushing, pulling, turning, controlling, activating, etc)? Was the task easy or demanding, slow or brisk, slack or strenuous, restful or laborious?";
+      scale[1] = '<?= gettext("Physical Demand") ?>';
+      left[1] = '<?= gettext("Low") ?>';
+      right[1] = '<?= gettext("High") ?>';
+      def[1] = '<?= gettext("How much physical activity was required (e.g. pushing, pulling, turning, controlling, activating, etc)? Was the task easy or demanding, slow or brisk, slack or strenuous, restful or laborious?") ?>';
 
-      scale[2] = "Temporal Demand";
-      left[2] = "Low";
-      right[2] = "High";
-      def[2] = "How much time pressure did you feel due to the rate of pace at which the tasks or task elements occurred? Was the pace slow and leisurely or rapid and frantic?";
+      scale[2] = '<?= gettext("Temporal Demand") ?>';
+      left[2] = '<?= gettext("Low") ?>';
+      right[2] = '<?= gettext("High") ?>';
+      def[2] = '<?= gettext("How much time pressure did you feel due to the rate of pace at which the tasks or task elements occurred? Was the pace slow and leisurely or rapid and frantic?") ?>';
 
-      scale[3] = "Performance";
-      left[3] = "Good";
-      right[3] = "Poor";
-      def[3] = "How successful do you think you were in accomplishing the goals of the task set by the experimenter (or yourself)? How satisfied were you with your performance in accomplishing these goals?";
+      scale[3] = '<?= gettext("Performance") ?>';
+      left[3] = '<?= gettext("Good") ?>';
+      right[3] = '<?= gettext("Poor") ?>';
+      def[3] = '<?= gettext("How successful do you think you were in accomplishing the goals of the task set by the experimenter (or yourself)? How satisfied were you with your performance in accomplishing these goals?") ?>';
 
-      scale[4] = "Effort";
-      left[4] = "Low";
-      right[4] = "High";
-      def[4] = "How hard did you have to work (mentally and physically) to accomplish your level of performance?";
+      scale[4] = '<?= gettext("Effort") ?>';
+      left[4] = '<?= gettext("Low") ?>';
+      right[4] = '<?= gettext("High") ?>';
+      def[4] = '<?= gettext("How hard did you have to work (mentally and physically) to accomplish your level of performance?") ?>';
 
-      scale[5] = "Frustration";
-      left[5] = "Low";
-      right[5] = "High";
-      def[5] = "How insecure, discouraged, irritated, stressed and annoyed versus secure, gratified, content, relaxed and complacent did you feel during the task?";
+      scale[5] = '<?= gettext("Frustration") ?>';
+      left[5] = '<?= gettext("Low") ?>';
+      right[5] = '<?= gettext("High") ?>';
+      def[5] = '<?= gettext("How insecure, discouraged, irritated, stressed and annoyed versus secure, gratified, content, relaxed and complacent did you feel during the task?") ?>';
 
-      var scale_error = 'A value must be selected for every scale!';
-      // -->
+      var scale_error = '<?= gettext("A value must be selected for every scale!") ?>';
     </script>
 
     <link rel="stylesheet" href="css/bootstrap.min.css">
@@ -78,7 +89,6 @@ require_once('config.php');?>
     <script src="js/jquery-1.10.2.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <script src="js/nasatlx.js"></script>
-
   </head>
 
 
@@ -111,11 +121,11 @@ require_once('config.php');?>
 
         <div id="div_part1">
 
-          <p class="lead">Click on each scale at the point that best indicates your experience of the task.</p>
+          <p class="lead"><?= gettext("Click on each scale at the point that best indicates your experience of the task.") ?></p>
 
-          <p>In other words: think back on what was easy and what was difficult or annoying about the task. How low or high were the mental demands, physical demands, etc... Drag your cursor over the question marks for more information.</p>
+          <p><?= gettext("In other words: think back on what was easy and what was difficult or annoying about the task. How low or high were the mental demands, physical demands, etc... Drag your cursor over the question marks for more information.") ?></p>
 
-          <p><strong>There are no right or wrong answers. If something is not entirely clear, follow your intuition.</strong></p>
+          <p><strong><?= gettext("There are no right or wrong answers. If something is not entirely clear, follow your intuition.") ?></strong></p>
 
           <div id="scale0"></div>
           <div id="scale1"></div>
@@ -125,52 +135,71 @@ require_once('config.php');?>
           <div id="scale5"></div>
 
           <br>
-          <input class="next btn btn-primary pull-right" id="next" type="button" value="Continue &gt;&gt;" onclick="buttonPart1();">
+          <input class="next btn btn-primary pull-right" id="next" type="button" value="<?= gettext("Next") ?> >>" onclick="buttonPart1();">
         </div>
 
         <div id="div_part2" style="display:none">
 
-          <p class="lead">One each of the following 15 screens, click on the scale title that represents the more important contributor to workload for the task.</p>
+          <p class="lead"><?= gettext("One each of the following 15 screens, click on the scale title that represents the more important contributor to workload for the task.") ?></p>
 
-          <p><strong>In other words: which factor made the task more tedious than the other?</strong></p>
+          <p><strong><?= gettext("In other words: which factor made the task more tedious than the other?") ?></strong></p>
 
           <br>
-          <input class="next btn btn-primary pull-right" id="next" type="button" value="Continue &gt;&gt;" onclick="buttonPart2();">
+          <input class="next btn btn-primary pull-right" id="next" type="button" value="<?= gettext("Next") ?> >>" onclick="buttonPart2();">
         </div>
 
         <div id="div_part3" style="display:none">
 
-          <p class="lead">Click on the factor that represents the more important contributor to workload for the task.</p>
+          <p class="lead"><?= gettext("Click on the factor that represents the more important contributor to workload for the task.") ?></p>
 
-          <p><strong>In other words: which factor made the task more tedious than the other?</strong></p>
+          <p><strong><?= gettext("In other words: which factor made the task more tedious than the other?") ?></strong></p>
 
           <br>
-          <table>
-            <tbody><tr>
-                <td><button class="pair btn btn-default btn-lg btn-block" id="pair1" type="button" onclick="buttonPair1();"></button></td>
-                <td class="def"><div id="pair1_def"></div></td>
-              </tr>
-              <tr>
-                <td align="center"> oder </td>
-                <td></td>
-              </tr>
-              <tr>
-                <td><button class="pair btn btn-default btn-lg btn-block" id="pair2" type="button" onclick="buttonPair2();"></button></td>
-                <td class="def"><div id="pair2_def"></div></td>
-              </tr>
-            </tbody></table>
+
+
+          <div class="row">
+            <div class="col-md-8">
+              <button class="pair btn btn-default btn-lg btn-block" id="pair1" type="button" onclick="buttonPair1();"></button>
+            </div>
+            <div class="col-md-4">
+              <div id="pair1_def"></div>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-md-8" style="text-align: center;">
+              <?= gettext("or") ?>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-md-8">
+              <button class="pair btn btn-default btn-lg btn-block" id="pair2" type="button" onclick="buttonPair2();"></button>
+            </div>
+            <div class="col-md-4">
+              <div id="pair2_def"></div>
+            </div>
+          </div>
+
         </div>
 
         <div id="div_part4" style="display:none">
-          <p class="lead">Results</p>
-          <div id="success" class="alert alert-success" style="display:none">
-            <strong>All done!</strong> Results submitted. Thank you for participating.
+          <p class="lead"><?= gettext("Results") ?></p>
+          <div id="success" style="display:none">
+            <div class="alert alert-success">
+                <strong><?= gettext("All done!") ?></strong> <?= gettext("Results submitted. Thank you for participating.") ?>
+            </div>
+
+            <?php if($showCloseButton) {?>
+            <a role="button" class="btn btn-primary btn-lg" href="javascript:window.open('','_self').close();"><?= gettext("Close") ?></a>
+            <?php } ?>
+
           </div>
           <div id="error" class="alert alert-danger" style="display:none">
-            <strong>Error!</strong> Something went wrong when submitting the results:<br/>
+            <strong><?= gettext("Error!") ?></strong> <?= gettext("Something went wrong when submitting the results:") ?><br/>
             <span id='error_msg'></span>
           </div>
-          <div id="results">
+          <div id="results" <?php if(!$showResults) {?> style="display:none" <?php } ?>>
           </div>
         </div>
 
